2eb16ddb4e7d5e0ea6c9ff702d67c64a41c757ca
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-03 09:21:32 -0400

    formatted the liri file

--- liri.js
+++ liri.js
@@ -11,9 +11,9 @@ var apiType = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
 
 
-if(apiType == "movie-this"){
+if (apiType == "movie-this") {
     axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy").then(
-        function(response){
+        function (response) {
 
             var movie = response.data;
 
@@ -21,14 +21,14 @@ if(apiType == "movie-this"){
         }
     );
 }
-else if(apiType == "spotify-this-song"){
-    spotifyAPI.search({type: "track", query: searchTerm, limit: 20}, function(err, data) {
+else if (apiType == "spotify-this-song") {
+    spotifyAPI.search({ type: "track", query: searchTerm, limit: 20 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
         var resultArray = data.tracks.items;
 
-        for(var i = 0; i < resultArray.length; i++){
+        for (var i = 0; i < resultArray.length; i++) {
             var band = resultArray[i].album.artists[0].name;
             var song = resultArray[i].name;
             var album = resultArray[i].album.name;
@@ -36,14 +36,14 @@ else if(apiType == "spotify-this-song"){
 
             console.log("\nSong Name: " + song + "\nBand: " + band + "\nAlbum: " + album + "\nSpotify Link: " + songLink);
         }
-      });
+    });
 }
-else if(apiType == "concert-this"){
-    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(function(response){
+else if (apiType == "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(function (response) {
         // console.log(response);
 
         var resultData = response.data;
-        for(var i = 0; i < resultData.length; i++){
+        for (var i = 0; i < resultData.length; i++) {
             var date = resultData[i].datetime;
             var venueName = resultData[i].venue.name;
             var venueCity = resultData[i].venue.city;

7e2557a688f1b3d8ff6662cdb7eb06f297acfadb
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-03 08:49:11 -0400

    added spotify api and bands api search functionality, spotify api displays cleanly, stil need to parse through the bands return data to display it properly

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
-var spotify = require("node-spotify-api");
+require("dotenv").config();
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var keys = require("./keys.js");
+
+var spotifyAPI = new Spotify(keys.spotify);
+
+// console.log(spotifyAPI);
 
 var apiType = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
@@ -16,9 +22,25 @@ if(apiType == "omdb"){
     );
 }
 else if(apiType == "spotify"){
+    spotifyAPI.search({type: "track", query: searchTerm, limit: 20}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        var resultArray = data.tracks.items;
 
+        for(var i = 0; i < resultArray.length; i++){
+            var band = resultArray[i].album.artists[0].name;
+            var song = resultArray[i].name;
+            var album = resultArray[i].album.name;
+            var songLink = resultArray[i].external_urls.spotify;
+
+            console.log("\nSong Name: " + song + "\nBand: " + band + "\nAlbum: " + album + "\nSpotify Link: " + songLink);
+        }
+      });
 }
 else if(apiType == "bands"){
-
+    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "?app_id=codingbootcamp").then(function(response){
+        console.log(response);
+    })
 }
 
